933724f825dafc89e5d56c776609a6221c66b5d4
samirouche <s.amirouche@hotmail.fr>
2017-06-02 15:35:10 +0200

    Add files via upload

--- graphe.m
+++ graphe.m
@@ -3,15 +3,17 @@ data_er = load('er.txt');
 data_li = load('li.txt');
 data_reset = load('reset.txt');
 
-plot(data_nf(:,2),log10(data_nf(:,1)),'-k','LineWidth',2);
+plot(data_nf(:,2),log10(data_nf(:,1)),'-k','LineWidth',3);
 hold on 
-plot(data_er(:,2),log10(data_er(:,1)),'-y','LineWidth',2);
+my_colour = [255 212 59] ./ 255;
+plot(data_er(:,2),log10(data_er(:,1)),'-','LineWidth',3,'Color',my_colour);
 hold on
-plot(data_li(:,2),log10(data_li(:,1)),'-g','LineWidth',2);
+plot(data_li(:,2),log10(data_li(:,1)),'-g','LineWidth',3);
 hold on 
-plot(data_reset(:,2),log10(data_reset(:,1)),'-r','LineWidth',2);
+plot(data_reset(:,2),log10(data_reset(:,1)),'-r','LineWidth',3);
 legend("NF","ER","LI","RESET");
 xlabel("Number of iterations")
 ylabel("convergence")
+title("Numerical behavior of recovery strategies with ULFM")
 
 print -djpg image2.jpg
